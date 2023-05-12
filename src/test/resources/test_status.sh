2120067e14a6950200e7c752c0ed9a38f917e75c
Mohd Jeeshan <mohdjeeshan007@gmail.com>
2023-05-12 14:03:18 +0200

    Update test_status.sh for monitoring to prometheus

--- src/test/resources/test_status.sh
+++ src/test/resources/test_status.sh
@@ -9,13 +9,13 @@ SUITE=$(awk '/suite/ { print $0 }' "$TEST_RESULTS_REPORT")
 #<suite started-at="2023-05-10T16:39:44 CEST" name="Suite1" finished-at="2023-05-10T16:39:50 CEST" duration-ms="5212"> </suite> <!-- Suite1 -->
 
 
-cat << EOF | curl --data-binary @- http://${PUSHGATEWAY_IP}:9091/metrics/job/selenium_job
+cat << EOF | curl --data-binary @- http://${PUSHGATEWAY_IP}:9091/metrics/job/results
 selenium_ignored_tests{action_id="${GITHUB_RUN_NUMBER}", author="${GITHUB_ACTOR}", os="${RUNNER_OS}"} $(echo ${TEST_REPORT} | awk -F'"' '{ print $2 }')
 selenium_total_tests{action_id="${GITHUB_RUN_NUMBER}", author="${GITHUB_ACTOR}", os="${RUNNER_OS}"} $(echo ${TEST_REPORT} | awk -F'"' '{ print $4 }')
 selenium_total_tests_passed{action_id="${GITHUB_RUN_NUMBER}", author="${GITHUB_ACTOR}", os="${RUNNER_OS}"} $(echo ${TEST_REPORT} | awk -F'"' '{ print $6 }')
 selenium_total_tests_failed{action_id="${GITHUB_RUN_NUMBER}", author="${GITHUB_ACTOR}", os="${RUNNER_OS}"} $(echo ${TEST_REPORT} | awk -F'"' '{ print $8 }')
 selenium_total_tests_skipped{action_id="${GITHUB_RUN_NUMBER}", author="${GITHUB_ACTOR}", os="${RUNNER_OS}"} $(echo ${TEST_REPORT} | awk -F'"' '{ print $10 }')
-selenium_suite_execution_duration{action_id="${GITHUB_RUN_NUMBER}", author="${GITHUB_ACTOR}", os="${RUNNER_OS}"} $(echo ${SUITE} | awk -F'"' '{ print $10 }')
+selenium_suite_execution_duration{action_id="${GITHUB_RUN_NUMBER}", author="${GITHUB_ACTOR}", os="${RUNNER_OS}"} $(echo ${SUITE} | awk -F'"' '{ print $8 }')
 EOF
 
 
